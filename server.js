fbda2204c66a1ab9cbd78e397a457510909d1620
not-archiey <notarchiey@protonmail.com>
2020-05-26 05:32:26 +0700

    Added server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv');
+
+const faker = require('faker');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get('/test', (req,res) => {
+    res.send(`Also, the API says hello to ${faker.name.firstName()}!`)
+})
+
+app.listen(port, () => console.log(`Listening on ${port}`));
